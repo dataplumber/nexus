cb67795a249e96cf1485f54d418a9bb012711c80
kmgill <kgill@jpl.nasa.gov>
2016-08-11 16:32:53 -0700

    Adding metadata to list results

--- analysis/webservice/algorithms/doms/DatasetListQuery.py
+++ analysis/webservice/algorithms/doms/DatasetListQuery.py
@@ -2,10 +2,12 @@ from webservice.NexusHandler import NexusHandler as BaseHandler
 from webservice.webmodel import StatsComputeOptions
 from webservice.NexusHandler import nexus_handler
 from webservice.NexusHandler import DEFAULT_PARAMETERS_SPEC
-from webservice.webmodel import NexusResults, NexusProcessingException
+from webservice.webmodel import NexusResults, NexusProcessingException, DatasetNotFoundException
 import BaseDomsHandler
 import datafetch
 import config
+import requests
+import json
 
 @nexus_handler
 class DomsDatasetListQueryHandler(BaseDomsHandler.BaseDomsQueryHandler):
@@ -19,16 +21,37 @@ class DomsDatasetListQueryHandler(BaseDomsHandler.BaseDomsQueryHandler):
     def __init__(self):
         BaseHandler.__init__(self)
 
+
+    def __getUrlForDataset(self, dataset):
+        datasetSpec = config.getEndpointByName(dataset)
+        if datasetSpec is not None:
+            return datasetSpec["metadataUrl"]
+        else:
+            return "http://doms.jpl.nasa.gov/ws/metadata/dataset?shortName=%s&format=umm-json"%dataset
+
+    def getMetadataForSource(self, dataset):
+        try:
+            r = requests.get(self.__getUrlForDataset(dataset))
+            results = json.loads(r.text)
+            return results
+        except:
+            return None
+
     def calc(self, computeOptions, **args):
 
         satellitesList = self._tile_service.get_dataseries_list()
 
         insituList = []
 
+        for satellite in satellitesList:
+            satellite["metadata"] = self.getMetadataForSource(satellite["shortName"])
+
+
         for insitu in config.ENDPOINTS:
             insituList.append({
                 "name" : insitu["name"],
-                "endpoint" : insitu["url"]
+                "endpoint" : insitu["url"],
+                "metadata": self.getMetadataForSource(insitu["name"])
             })
 
 
