5227774efab30556b80bf6ceed715991c85462dd
Frank Greguska <francis.greguska@jpl.nasa.gov>
2016-10-05 11:02:10 -0700

    Results from data series list was a list. Needs to be an object with toJson method. fixed.

--- analysis/webservice/algorithms/DataSeriesList.py
+++ analysis/webservice/algorithms/DataSeriesList.py
@@ -2,6 +2,7 @@
 Copyright (c) 2016 Jet Propulsion Laboratory,
 California Institute of Technology.  All rights reserved
 """
+import json
 from webservice.NexusHandler import NexusHandler
 from webservice.NexusHandler import nexus_handler
 from webservice.webmodel import cached
@@ -9,7 +10,6 @@ from webservice.webmodel import cached
 
 @nexus_handler
 class DataSeriesListHandlerImpl(NexusHandler):
-
     name = "Dataset List"
     path = "/list"
     description = "Lists datasets currently available for analysis"
@@ -20,4 +20,11 @@ class DataSeriesListHandlerImpl(NexusHandler):
 
     @cached(ttl=(60 * 60 * 1000))  # 1 hour cached
     def calc(self, computeOptions, **args):
-        return self._tile_service.get_dataseries_list()
+        class SimpleResult(object):
+            def __init__(self, result):
+                self.result = result
+
+            def toJson(self):
+                return json.dumps(self.result)
+
+        return SimpleResult(self._tile_service.get_dataseries_list())
