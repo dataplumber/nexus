b925effbb6ea1315634b8481d73628188b06833f
Nga Quach <Nga.T.Chung@jpl.nasa.gov>
2016-10-06 14:12:29 -0700

    Updated groovy script to not return list of section spec.

--- nexus-ingest/groovy-scripts/add-time-to-spatial-spec.groovy
+++ nexus-ingest/groovy-scripts/add-time-to-spatial-spec.groovy
@@ -6,17 +6,15 @@
  * Created by Nga Quach on 8/31/16.
  */
 
-def sectionSpecList = []
 def sectionSpec = ''
 
 for (i = 0; i < 240; i++) {
     sectionSpec <<= 'time:' + i + ':' + (i+1) + ','
-    sectionSpec <<= payload.join(';time:' + i + ':' + (i+1) + ',')
+    sectionSpec <<= payload
     sectionSpec <<= ';'
-    sectionSpec <<= 'file://'
-    sectionSpec <<= headers.absolutefilepath
-    sectionSpecList.add(sectionSpec.toString())
-    sectionSpec.length = 0
 }
 
-return sectionSpecList
+sectionSpec <<= 'file://'
+sectionSpec <<= headers.absolutefilepath
+
+return sectionSpec.toString()
