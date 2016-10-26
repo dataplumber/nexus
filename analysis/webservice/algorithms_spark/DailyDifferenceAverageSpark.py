9e5c5d570e72113647e21c83240bbc9e8a0de164
Frank Greguska <francis.greguska@jpl.nasa.gov>
2016-10-25 18:09:29 -0700

    oxt-19 first stab at spark daily difference average algorithm

--- analysis/webservice/algorithms_spark/DailyDifferenceAverageSpark.py
+++ analysis/webservice/algorithms_spark/DailyDifferenceAverageSpark.py
@@ -3,17 +3,14 @@ Copyright (c) 2016 Jet Propulsion Laboratory,
 California Institute of Technology.  All rights reserved
 """
 import logging
-import sys
-import traceback
 from datetime import datetime, timedelta
-from multiprocessing.dummy import Pool, Manager
 
 import numpy as np
 import pytz
 from nexustiles.nexustiles import NexusTileService
 from shapely import wkt
 
-from webservice.NexusHandler import NexusHandler, nexus_handler, SparkHandler
+from webservice.NexusHandler import nexus_handler, SparkHandler
 from webservice.webmodel import NexusResults, NexusProcessingException
 
 SENTINEL = 'STOP'
@@ -28,7 +25,7 @@ def iso_time_to_epoch(str_time):
 
 @nexus_handler
 class DailyDifferenceAverageSparkImpl(SparkHandler):
-    name = "Daily Difference Average"
+    name = "Daily Difference Average Spark"
     path = "/dailydifferenceaverage_spark"
     description = "Subtracts data in box in Dataset 1 from Dataset 2, then averages the difference per day."
     params = {
@@ -128,7 +125,7 @@ class DailyDifferenceAverageSparkImpl(SparkHandler):
             stats={},
             meta=self.get_meta())
 
-        min_lon, min_lat, max_lon, max_lat = bounding_polygon.bounds[0]
+        min_lon, min_lat, max_lon, max_lat = bounding_polygon.bounds
         result.extendMeta(min_lat, max_lat, min_lon, max_lon, "", start_time, end_time)
         result.meta()['label'] = u'Difference from 5-Day mean (\u00B0C)'
 
@@ -147,52 +144,6 @@ class DailyDifferenceAverageSparkImpl(SparkHandler):
         }
         return meta
 
-    def get_daily_difference_average_for_box(self, min_lat, max_lat, min_lon, max_lon, dataset1, dataset2,
-                                             start_time,
-                                             end_time):
-
-        daysinrange = self._tile_service.find_days_in_range_asc(min_lat, max_lat, min_lon, max_lon, dataset1,
-                                                                start_time, end_time)
-
-        maxprocesses = int(self.algorithm_config.get("multiprocessing", "maxprocesses"))
-
-        if maxprocesses == 1:
-            calculator = DailyDifferenceAverageCalculator()
-            averagebyday = []
-            for dayinseconds in daysinrange:
-                result = calculator.calc_average_diff_on_day(min_lat, max_lat, min_lon, max_lon, dataset1, dataset2,
-                                                             dayinseconds)
-                averagebyday.append((result[0], result[1]))
-        else:
-            # Create a task to calc average difference for each day
-            manager = Manager()
-            work_queue = manager.Queue()
-            done_queue = manager.Queue()
-            for dayinseconds in daysinrange:
-                work_queue.put(
-                    ('calc_average_diff_on_day', min_lat, max_lat, min_lon, max_lon, dataset1, dataset2, dayinseconds))
-            [work_queue.put(SENTINEL) for _ in xrange(0, maxprocesses)]
-
-            # Start new processes to handle the work
-            pool = Pool(maxprocesses)
-            [pool.apply_async(pool_worker, (work_queue, done_queue)) for _ in xrange(0, maxprocesses)]
-            pool.close()
-
-            # Collect the results as [(day (in ms), average difference for that day)]
-            averagebyday = []
-            for i in xrange(0, len(daysinrange)):
-                result = done_queue.get()
-                if result[0] == 'error':
-                    print >> sys.stderr, result[1]
-                    raise NexusProcessingException(reason="Error calculating average by day.")
-                rdata = result
-                averagebyday.append((rdata[0], rdata[1]))
-
-            pool.terminate()
-            manager.shutdown()
-
-        return averagebyday
-
 
 from threading import Lock
 import operator
@@ -200,7 +151,7 @@ import operator
 DRIVER_LOCK = Lock()
 
 
-def spark_anomolies_driver(self, tile_ids, bounding_wkt, dataset, climatology, sc=None):
+def spark_anomolies_driver(tile_ids, bounding_wkt, dataset, climatology, sc=None):
     from functools import partial
 
     with DRIVER_LOCK:
@@ -212,20 +163,42 @@ def spark_anomolies_driver(self, tile_ids, bounding_wkt, dataset, climatology, s
         rdd = sc.parallelize(tile_ids)
 
     def add_tuple_elements(tuple1, tuple2):
-        tuple(map(operator.add, tuple1, tuple2))
+        return tuple(map(operator.add, tuple1, tuple2))
 
     result = rdd \
-        .mapPartitions(partial(calculate_diff, tile_ids=tile_ids, bounding_wkt=bounding_wkt_b, dataset=dataset_b,
+        .mapPartitions(partial(calculate_diff, bounding_wkt=bounding_wkt_b, dataset=dataset_b,
                                climatology=climatology_b)) \
         .reduceByKey(add_tuple_elements) \
-        .mapValues(operator.div) \
+        .mapValues(lambda x: x[0]/x[1]) \
         .sortByKey() \
-        .collectAsMap()
+        .collect()
 
     return result
 
 
+def generate_diff(data_tile, climatology_tile):
+    the_time = datetime.now()
+    # Subtract climatology tile from data tile
+    diff = np.ma.subtract(data_tile.data, climatology_tile.data)
+    diff_ct = np.ma.count(diff)
+    diff_sum = np.ma.sum(diff)
+    date_in_seconds = int((
+        datetime.combine(data_tile.min_time.date(), datetime.min.time()).replace(
+            tzinfo=pytz.UTC) - EPOCH).total_seconds())
+
+    print "%s Time to generate diff between %s and %s" % (
+        str(datetime.now() - the_time), data_tile.tile_id, climatology_tile.tile_id)
+
+    yield (date_in_seconds, (diff_sum, diff_ct))
+
+
 def calculate_diff(tile_ids, bounding_wkt, dataset, climatology):
+    from shapely.geometry import box
+    from itertools import chain
+
+    # Construct a list of generators that yield (day, sum, count)
+    diff_generators = []
+
     tile_ids = list(tile_ids)
     if len(tile_ids) == 0:
         return []
@@ -236,76 +209,30 @@ def calculate_diff(tile_ids, bounding_wkt, dataset, climatology):
         try:
             the_time = datetime.now()
             dataset_tile = tile_service.mask_tiles_to_polygon(wkt.loads(bounding_wkt.value),
-                                                      tile_service.find_tile_by_id(tile_id))[0]
-            print "%s Time to load tile %s" % (str(datetime.now() - the_time), tile_id)
-        except IndexError:
-            # This should only happen if all measurements in a tile become masked after applying the bounding polygon
-            raise StopIteration
-
-        # Load the climatology tile
-        try:
-            the_time = datetime.now()
-            dataset_tile = tile_service.mask_tiles_to_polygon(wkt.loads(bounding_wkt.value),
                                                               tile_service.find_tile_by_id(tile_id))[0]
             print "%s Time to load tile %s" % (str(datetime.now() - the_time), tile_id)
         except IndexError:
             # This should only happen if all measurements in a tile become masked after applying the bounding polygon
-            raise StopIteration
+            continue
 
+        tile_day_of_year = dataset_tile.min_time.timetuple().tm_yday
 
-class DailyDifferenceAverageCalculator(object):
-    def __init__(self):
-        self.__tile_service = NexusTileService()
+        # Load the climatology tile
+        try:
+            the_time = datetime.now()
+            climatology_tile = tile_service.mask_tiles_to_polygon(wkt.loads(bounding_wkt.value),
+                                                                  tile_service.find_tile_by_polygon_and_most_recent_day_of_year(
+                                                                      box(dataset_tile.bbox.min_lon,
+                                                                          dataset_tile.bbox.min_lat,
+                                                                          dataset_tile.bbox.max_lon,
+                                                                          dataset_tile.bbox.max_lat),
+                                                                      climatology.value,
+                                                                      tile_day_of_year))[0]
+            print "%s Time to load climatology tile %s" % (str(datetime.now() - the_time), tile_id)
+        except IndexError:
+            # This should only happen if all measurements in a tile become masked after applying the bounding polygon
+            continue
 
-    def calc_average_diff_on_day(self, min_lat, max_lat, min_lon, max_lon, dataset1, dataset2, timeinseconds):
+        diff_generators.append(generate_diff(dataset_tile, climatology_tile))
 
-        day_of_year = datetime.fromtimestamp(timeinseconds, pytz.utc).timetuple().tm_yday
-
-        ds1_nexus_tiles = self.__tile_service.find_all_tiles_in_box_at_time(min_lat, max_lat, min_lon, max_lon,
-                                                                            dataset1,
-                                                                            timeinseconds)
-
-        # Initialize list of differences
-        differences = []
-        # For each ds1tile
-        for ds1_tile in ds1_nexus_tiles:
-            # Get tile for ds2 using bbox from ds1_tile and day ms
-            try:
-                ds2_tile = self.__tile_service.find_tile_by_bbox_and_most_recent_day_of_year(ds1_tile.bbox.min_lat,
-                                                                                             ds1_tile.bbox.max_lat,
-                                                                                             ds1_tile.bbox.min_lon,
-                                                                                             ds1_tile.bbox.max_lon,
-                                                                                             dataset2, day_of_year)[0]
-                # Subtract ds2 tile from ds1 tile
-                diff = np.subtract(ds1_tile.data, ds2_tile.data)
-            except IndexError:
-                # This happens when there is data in ds1tile but all NaNs in ds2tile because the
-                # Solr query being used filters out results where stats_count = 0.
-                # Technically, this should never happen if ds2 is a climatology generated in part from ds1
-                # and it is probably a data error
-
-                # For now, just treat ds2 as an array of all masked data (which essentially discards the ds1 data)
-                ds2_tile = np.ma.masked_all(ds1_tile.data.shape)
-                diff = np.subtract(ds1_tile.data, ds2_tile)
-
-            # Put results in list of differences
-            differences.append(np.ma.array(diff).ravel())
-
-        # Average List of differences
-        diffaverage = np.ma.mean(differences).item()
-        # Return Average by day
-        return int(timeinseconds), diffaverage
-
-
-def pool_worker(work_queue, done_queue):
-    try:
-        calculator = DailyDifferenceAverageCalculator()
-
-        for work in iter(work_queue.get, SENTINEL):
-            scifunction = work[0]
-            args = work[1:]
-            result = calculator.__getattribute__(scifunction)(*args)
-            done_queue.put(result)
-    except Exception as e:
-        e_str = traceback.format_exc(e)
-        done_queue.put(('error', e_str))
+    return chain(*diff_generators)
