17b8616573b625acff45f88e28c511877d279fc4
kmgill <kgill@jpl.nasa.gov>
2017-03-08 16:14:58 -0800

    Additions to dataset details handler. Might merge some of this one with DatasetList due to some redundancies.

--- analysis/webservice/algorithms/DatasetDetails.py
+++ analysis/webservice/algorithms/DatasetDetails.py
@@ -11,16 +11,43 @@ class DatasetDetailsHandler(BaseHandler):
     name = "DatasetDetailsHandler"
     path = "/datasetDetails"
     description = ""
-    params = {}
+    params = {
+        "ds": {
+            "name": "Dataset",
+            "type": "string",
+            "description": "A supported dataset shortname identifier. If omitted, all datasets will be returned."
+        }
+    }
     singleton = True
 
     def __init__(self):
         BaseHandler.__init__(self)
 
+
+    def __get_dataset_details(self, ds):
+        stats = self._tile_service.get_dataset_overall_stats(ds)
+        return stats
+
+    """
+        This one will take a while...
+    """
+    def __get_all_dataset_details(self):
+        ds_list = self._tile_service.get_dataseries_list(simple=True)
+        stats = []
+        for ds in ds_list:
+            print ds["shortName"]
+            ds_stats = self.__get_dataset_details(ds["shortName"])
+            stats.append({"id": ds["shortName"], "stats": ds_stats})
+
+        return stats
+
     def calc(self, computeOptions, **args):
         ds = computeOptions.get_argument("ds", None)
 
-        stats = self._tile_service.get_dataset_overall_stats(ds)
+        if ds is not None:
+            stats = self.__get_dataset_details(ds)
+        else:
+            stats = self.__get_all_dataset_details()
 
         class SimpleResult(object):
             def toJson(self):
