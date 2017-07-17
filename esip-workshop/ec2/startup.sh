2fbd61f3d5d033aae2768b7bfde817ca2e083c9a
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-07-17 11:57:29 -0700

    added work directory

--- esip-workshop/ec2/startup.sh
+++ esip-workshop/ec2/startup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -d -p 8000:8888 -v /home/ndeploy/workshop1/:/home/jovyan --network infrastructure_nexus --name jupyter nexusjpl/jupyter start-notebook.sh --NotebookApp.token='' --NotebookApp.allow_origin='*'
+docker run -d -p 8000:8888 -v /Users/greguska/githubprojects/nexus/esip-workshop/student-material/:/home/jovyan --network infrastructure_nexus --name jupyter nexusjpl/jupyter start-notebook.sh --NotebookApp.token='' --NotebookApp.allow_origin='*'
