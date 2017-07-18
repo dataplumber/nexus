780b460462706817532ce52e169a06b3ce0f8888
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-07-18 10:40:54 -0700

    updated startup script

--- esip-workshop/ec2/startup.sh
+++ esip-workshop/ec2/startup.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-docker run -d -p 8000:8888 -v /home/ndeploy/nexus/esip-workshop/student-material:/home/jovyan --network infrastructure_nexus --name jupyter nexusjpl/jupyter start-notebook.sh --NotebookApp.token='' --NotebookApp.allow_origin='*'
+cd /home/ndeploy/nexus/esip-workshop/docker/infrastructure
+docker-compose up -d cassandra1
+
+sleep 15
+
+docker-compose up -d
+
+sleep 30
+
+cd /home/ndeploy/nexus/esip-workshop/docker/analysis
+docker-compose up -d
+
+docker run -d -p 8000:8888 -v /home/ndeploy/nexus/esip-workshop/student-material:/home/jovyan --network infrastructure_nexus --name jupyter nexusjpl/jupyter start-notebook.sh --NotebookApp.password='sha1:1b994085c83b:05d658b3866739ee07f5cb52a67474cdc898840e' --NotebookApp.allow_origin='*'
