19a3c0fdd5de0df3797d2a86833e478bd98748ed
David Silvan <david.j.silvan@jpl.nasa.gov>
2017-12-20 10:52:13 -0800

    Fixed docker-entrypoint

--- docker/nexus-imaging/docker-entrypoint.sh
+++ docker/nexus-imaging/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 #Activate Python environment
 source activate nexus
 
-cd /tmp/image-gen-nexus/analysis/webservice
+cd /tmp/nexus/analysis/webservice
 python WorkflowDriver.py --ds ${DATASET_NAME} --g ${GRANULE_NAME} --p ${PREFIX} --ct ${COLOR_TABLE} --min ${MIN} --max ${MAX} --h ${HEIGHT} --w ${WIDTH} --t ${TIME_INTERVAL} --i ${INTERP}
 
